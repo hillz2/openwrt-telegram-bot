4ca2391272c991e5e64d8afde2d0046bea09c21e
hillz2 <galih.prastowo@gmail.com>
2024-12-04 20:32:40 +0700

    Update crypto.sh

--- usr/lib/telegram-bot/plugins/crypto.sh
+++ usr/lib/telegram-bot/plugins/crypto.sh
@@ -3,5 +3,7 @@
 if [[ ! -z $1 ]]; then
 	crypto.py -b "$1" | sed 's/\[1m//g;s/\[0m//g'
 else
+	crypto.py -b 400000 | sed 's/\[1m//g;s/\[0m//g'
+	echo "=========="
 	echo "Usage: /crypto [btc_value_in_usd]"
 fi
