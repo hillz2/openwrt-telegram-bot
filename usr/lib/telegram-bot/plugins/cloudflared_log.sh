0e14454ca10a6e09bf0655b2f81056a91db26a1d
hillz2 <galih.prastowo@gmail.com>
2025-01-30 10:32:53 +0700

    Update cloudflared_log.sh

--- usr/lib/telegram-bot/plugins/cloudflared_log.sh
+++ usr/lib/telegram-bot/plugins/cloudflared_log.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker logs --tail 5 cloudflared 2>&1 | sed 's/[^a-zA-Z0-9_ -]//g'
+if [ -f /etc/init.d/cloudflared ]; then
+        tail -n 10 /var/log/cloudflared.err | sed 's/[^a-zA-Z0-9_ -]//g'
+else
+        docker logs --tail 5 cloudflared 2>&1 | sed 's/[^a-zA-Z0-9_ -]//g'
+fi
